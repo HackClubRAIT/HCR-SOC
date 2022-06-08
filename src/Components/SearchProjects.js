09aa27006bd334c2e55f13396f88263aca15f3e1
Chaitalish02 <chaitalishetty261@gmail.com>
2022-06-08 23:00:31 +0530

    project section

---
+++ src/Components/SearchProjects.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+import {proj} from '../backend/projects'
+import ProjectCards from './ProjectCards'
+function SearchProjects() {
+     
+    const [searchProj,setsearchProj]=useState('')
+    // const searchproj=e=>{
+    //       setsearchProj(e.target.value)
+    // };
+    
+    return (
+    <div>
+        <div className="searchBar">
+        <input type="search" className="search" onChange={e=>setsearchProj(e.target.value)} placeholder="Search for projects or tech stack "/>
+        </div>
+        <div className='projects'>
+        {
+            proj.filter((val)=>{
+                
+                //convert json tech stack to lowercase
+                const lower = val.tech_stack.map((val)=>val.toLowerCase());
+                //check if input is equal to lower
+                const check=lower.map((val)=>val.includes(searchProj.toLowerCase()));
+                if(searchProj === ""){
+                    return val
+                }
+                else if(val.name.split("/")[1].toLowerCase().includes(searchProj.toLowerCase()))
+                {
+                    return val;
+                }
+                else if(check.includes(true)){
+                    return val;
+                }
+            }).map((val,key)=>{
+                return <ProjectCards name={val.name} tech_stack={val.tech_stack} desc={val.desc}/>
+            })
+        }
+        </div>
+        
+    </div>
+  )
+}
+
+export default SearchProjects
---
+++ src/backend/projects.js
@@ -0,0 +1,58 @@
+export const proj=[
+        {
+            "name":"HackClubRAIT/HackClubRAIT.github.io",
+            "tech_stack":["Html","Css","Javascript"],
+            "proj_admin":"HackClubRAIT",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"HackClub RAIT focuses essentially on open-source. Our aim is to flourish the thrilling open-source culture to create a favorable environment for open-source contributions."
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html","Python","API"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        },
+        {
+            "name":"Chaitalishetty/Covid19-tracker.github.io",
+            "tech_stack":["ReactJS","Css","Html"],
+            "proj_admin":"Chaitalishetty",
+            "mentor_mail":"chaitalishetty261@gmail.com",
+            "   mentor_linkedin":"https://www.linkedin.com/in/chaitali-shetty-b198491b4/",
+            "desc":"Covid tracker is used to track live cases in your country"
+        }
+    ]
