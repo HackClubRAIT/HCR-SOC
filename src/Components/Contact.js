f4b12f4de94abc39c18201f8b217524a4649260d
Chaitalish02 <chaitalishetty261@gmail.com>
2022-06-15 01:28:04 +0530

    contact and registe

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import MailIcon from '@mui/icons-material/Mail';
+import '../css/contact.css'
 function Contact() {
   return (
-    <div></div>
+    <>
+    <div style={{paddingTop:"5vh",height:"90vh"}} id="Contact Us">
+    <h1 className='headings'>Contact</h1>
+    <div className='contact'>
+      <div className='cgmail'>
+        <a href="mailto:hackclubraitsoc@gmail.com" className='gm' target="_blank_"><MailIcon sx={{fontSize:100 }}/></a>
+      </div>
+      <div className='cinsta'>
+      <a href="https://www.instagram.com/hackclubrait/" className='in' target="_blank_"><InstagramIcon sx={{fontSize:100}}/></a>
+      </div>
+      <div className='clinkedin'>
+      <a href="https://www.linkedin.com/company/hack-club-rait/mycompany/" className='li' target="_blank_"><LinkedInIcon sx={{fontSize:100}}/></a>
+      </div>
+      <div className='cgithub'>
+      <a href="https://github.com/HackClubRAIT" className='gi' target="_blank_"><GitHubIcon sx={{fontSize:100}}/></a>
+      </div>
+      <div className='ctwitter'>
+      <a href="https://twitter.com/RaitHackclub" className='tw' target="_blank_"><TwitterIcon sx={{fontSize:100}}/></a>
+
+      </div>
+      </div>
+
+    </div>
+    </>
   )
 }
 
---
+++ src/Components/Register.js
@@ -0,0 +1,30 @@
+import { Button, Card, Divider } from '@mui/material'
+import React from 'react'
+import '../css/register.css'
+function Register() {
+  return (
+    <div style={{paddingTop:"10vh"}}  id="Register">
+    <h1 className='headings'>Register</h1>
+        <div className='register'>
+            <Card variant='outlined' className='reg_card'>
+                <h1>Participant</h1><br/>
+                <Divider/><br/>
+                <p style={{fontSize:"30px"}}>If you want to contribute to the opensource of some amazing projects, then register here</p><br/>
+                <a href="https://forms.gle/mhfhZzh7cySuGakH7" target="_blank_" style={{textDecoration:"none"}}>
+                    <Button variant='contained' color='primary'> Register here</Button>
+                </a>
+            </Card>
+            <Card variant='outlined' className='reg_card'>
+                <h1>Project Maintainer</h1><br/>
+                <Divider/><br/>
+                <p style={{fontSize:"30px"}}>If you have some amazing projects which you want to open-source, then register here</p><br/>
+                <a href="#" style={{textDecoration:"none"}} target="_blank_">
+                <Button variant='contained' color='primary'> Register here</Button>
+                </a>
+            </Card>        
+        </div>
+    </div>
+  )
+}
+
+export default Register
